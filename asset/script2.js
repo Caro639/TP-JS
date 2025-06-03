7357ab83e587b70ea123904658ac01a498df2735
Caro639 <carolshine@hotmail.fr>
2025-06-03 11:29:13 +0200

    duel ok

--- asset/script2.js
+++ asset/script2.js
@@ -13,11 +13,13 @@ duel.addEventListener("submit", function (event) {
 
   let sorcier1Value = sorcier1.value;
 
-  sorcier1Value = document.createElement("p");
+  sorcier1Value = document.createElement("p", {
+    class: "sorcier1",
+  });
 
   sorcier1Value.innerHTML = sorcier1;
 
-  duelContent.insertAdjacentElement("beforeend", sorcier1Value);
+  duelContent.insertAdjacentElement("afterbegin", sorcier1Value);
   console.log(sorcier1);
 
   // maison1
@@ -25,7 +27,9 @@ duel.addEventListener("submit", function (event) {
 
   let maison1Value = maison1.value;
 
-  maison1Value = document.createElement("p");
+  maison1Value = document.createElement("p", {
+    class: "sorcier1",
+  });
 
   maison1Value.innerHTML = maison1;
 
@@ -37,7 +41,9 @@ duel.addEventListener("submit", function (event) {
 
   let sorcier2Value = sorcier2.value;
 
-  sorcier2Value = document.createElement("p");
+  sorcier2Value = document.createElement("p", {
+    class: "sorcier2",
+  });
 
   sorcier2Value.innerHTML = sorcier2;
 
@@ -49,10 +55,81 @@ duel.addEventListener("submit", function (event) {
 
   let maison2Value = maison2.value;
 
-  maison2Value = document.createElement("p");
+  maison2Value = document.createElement("p", {
+    class: "sorcier2",
+  });
 
   maison2Value.innerHTML = maison2;
 
   duelContent.insertAdjacentElement("beforeend", maison2Value);
   console.log(maison2);
+
+  // Afficher le duel
+  let PV1 = 200; // Vie du sorcier 1 à 200 points de vie
+  let PV2 = 200; //vie du sorcier 2
+  let tours = 0; // Compteur de tours
+
+  console.log(PV1, PV2);
+  // degats = attaque
+
+  while (PV2 > 0 && PV1 > 0) {
+    tours++;
+
+    console.log(tours);
+
+    // Attaque du sorcier 1
+    let attaque = Math.floor(Math.random() * 10) + 5; // Attaque du sorcier 1 entre 10 et 15
+
+    PV2 -= attaque;
+
+    // Afficher les dégâts et les points de vie restants après l'attaque du sorcier 1
+    let historique = document.querySelector(".historique");
+
+    historique.insertAdjacentHTML(
+      "beforeend",
+      `<p>Duel ${tours} : ${sorcier1} attaque ${attaque} à ${sorcier2}. Il reste ${PV2} points de vie à ${sorcier2}.</p>`
+    );
+
+    console.log(
+      `Duel : ${tours} Sorcier 1 attaque ${attaque}, PV restants: ${PV2}`
+    );
+
+    if (PV2 <= 0) {
+      historique.insertAdjacentHTML(
+        "beforeend",
+        `<p>${sorcier2} a perdu en ${tours} duels !</p>`
+      );
+      console.log("Sorcier 2 a perdu !");
+      break;
+    }
+
+    // Attaque du sorcier 2
+    tours++;
+    console.log(tours);
+    attaque = Math.floor(Math.random() * 10) + 5; // Nouvelle attaque pour le sorcier 2
+
+    PV1 -= attaque;
+
+    // Afficher les dégâts et les points de vie restants
+    historique.insertAdjacentHTML(
+      "beforeend",
+      `<p>Duel ${tours} : ${sorcier2} attaque ${attaque} à ${sorcier1}. Il reste ${PV1} points de vie à ${sorcier1}</p>`
+    );
+
+    console.log(
+      `Duel : ${tours} Sorcier 2 attaque ${attaque}, PV restants: ${PV1}`
+    );
+
+    if (PV1 <= 0) {
+      historique.insertAdjacentHTML(
+        "beforeend",
+        `<p>${sorcier1} a perdu en ${tours} duels !</p>`
+      );
+      console.log("Sorcier 1 a perdu !");
+      break;
+    }
+  }
 });
+// faire une condition while pour gérer le duel tant que le sorcier n'a pas zéro de vie
+//une seule fois à 50% de vie le sorcier peut reprendre 200 PV
+// si c'est le cas afficher cette ligne en vert ajouter une classe
